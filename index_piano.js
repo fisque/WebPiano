0f3fbf5148631d7ea768363570845b3498a2906b
Jenny <fisquell@gmail.com>
2018-02-25 16:08:58 -0500

    clicking on the keys no longer plays music: YOU MUST USE YOUR KEYBOARD! muhaha

--- index_piano.js
+++ index_piano.js
@@ -1,31 +1,3 @@
-function play(char) {
-  var key = char.charCodeAt(0);
-  if (key == 65 || key == 97) { //A, or middle C note
-    document.getElementById('noteC').play();
-  }
-  if (key == 83 || key == 115) { //S, or D note
-    document.getElementById('noteD').play();
-  }
-  if (key == 68 || key == 100) { //D, or E note
-    document.getElementById('noteE').play();
-  }
-  if (key == 70 || key == 102) { //F, or F note
-    document.getElementById('noteF').play();
-  }
-  if (key == 74 || key == 106) { //J, or G note
-    document.getElementById('noteG').play();
-  }
-  if (key == 75 || key == 107) { //K, or A note
-    document.getElementById('noteA').play();
-  }
-  if (key == 76 || key == 108) { //L, or B note
-    document.getElementById('noteB').play();
-  }
-  if (key == 58 || key == 59) { //:, or high C note
-    document.getElementById('noteHighC').play();
-  }
-}
-
 var song = [];
 var recorded = [];
 
