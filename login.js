b1dbe3b3d05e5592ea1bff8ba0ef38e4f1f00e76
Jenny <fisquell@gmail.com>
2018-02-25 15:18:10 -0500

    login validation works!

--- login.js
+++ login.js
@@ -2,13 +2,14 @@ function validate(){
 	/*Username: Jenny, Password: VirtualPiano*/
 	var username = document.getElementById("username").value;
 	var pwd = document.getElementById("pswrd").value;
-	console.log(pwd);	
+	console.log(pwd);
 	if(username == "Jenny" && pwd == "VirtualPiano"){
 		window.location = "profile.html";
-		return false;
-	} 
+		return true;
+	}
 	//alert("Your username or password was not correct");
-	document.getElementById("pwd-msg").innerHTML += "Your username or password was not correct";
+	document.getElementById("pwd-msg").style.display = 'inline';
+	return false;
 }
 
 function showPwd(){
