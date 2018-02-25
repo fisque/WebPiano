ec3d8fb156efc6689b99c4978699760af798d98b
Grace Huang <gmh9hz@virginia.edu>
2018-02-25 15:06:04 -0500

    add show password

--- login.js
+++ login.js
@@ -7,5 +7,16 @@ function validate(){
 		window.location = "profile.html";
 		return false;
 	} 
-	document.getElementById("pwd-msg").innerHTML = "Your username or password was not correct";
+	//alert("Your username or password was not correct");
+	document.getElementById("pwd-msg").innerHTML += "Your username or password was not correct";
+}
+
+function showPwd(){
+	var pwd = document.getElementById("pswrd");
+	if(pwd.type === "password"){
+		pwd.type = "text";
+	}
+	else{
+		pwd.type = "password";
+	}
 }
